80ef196199bb7583328b121fe06fb2054a04e8fb
D4Vinci <d4vinci@protonmail.com>
2016-12-27 22:56:49 +0200

    Upload the basic files..

---
+++ wormy.py
@@ -0,0 +1,10 @@
+#!C:\Python35\python3.exe #Because I have many versions on my pc :D
+# ToDo list :
+#   1)Spread in drivers,add to startup and continueos check (✘)
+#   2)Spread in scripts by injecting [py/pl/ps1...]         (✘)
+#   3)Spread in shared folders                              (✘)
+#   4)Spread in lan                                         (✘)
+#   5)Spread in usb                                         (✘)
+#   6)Make a copy of itself online and use it in spreading  (✘)
+#   7)[Advanced]Convert itself to executable and inject..   (✘)
+#   8)...Still thinking :D
